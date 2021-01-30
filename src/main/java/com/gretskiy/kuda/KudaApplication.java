68ea2e1101fd1b581d81ba54c936fdccd9175516
Arkadiy <greslayer@gmail.com>
2021-01-30 16:07:52 +0300

    Initial commit

---
+++ src/main/java/com/gretskiy/kuda/KudaApplication.java
@@ -0,0 +1,13 @@
+package com.gretskiy.kuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gretskiy/kuda/KudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gretskiy.kuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
